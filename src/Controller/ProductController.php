04f0b8e82311c484f4906be92192182c10fe88de
Mohamed Hanini <mohamed.hanini95@gmail.com>
2021-11-10 22:02:07 +0100

    create funtion  [ show product by id  ] => done

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Entity\Product;
 
 class ProductController extends AbstractController
@@ -13,17 +13,21 @@ class ProductController extends AbstractController
     /**
      * @Route("/product", name="product")
      */
-    public function createProduct(): Response
+    public function createProduct(ValidatorInterface $validator): Response
     {
         // you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
         $entityManager = $this->getDoctrine()->getManager();
 
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(1999);
+        $product->setName("test");
+        $product->setPrice('1999');
         $product->setDescription('Ergonomic and stylish!');
 
+        $errors = $validator->validate($product);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($product);
 
@@ -31,5 +35,26 @@ class ProductController extends AbstractController
         $entityManager->flush();
 
         return new Response('Saved new product with id '.$product->getId());
+    }
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show(int $id): Response
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
     }
 }
